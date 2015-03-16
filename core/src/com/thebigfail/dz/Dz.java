966c10cfea339788564c605997f2003b1a0d5176
geniusunil <geniusunil@gmail.com>
2015-03-16 13:42:39 +0530

    scrollable background, gesture to scroll is touch only

--- core/src/com/thebigfail/dz/Dz.java
+++ core/src/com/thebigfail/dz/Dz.java
@@ -3,25 +3,39 @@ package com.thebigfail.dz;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
 public class Dz extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	OrthographicCamera camera;
+    int cameraX;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("bg.jpg");
-	}
+        camera = new OrthographicCamera(720,1280);
+        camera.position.set(1080, 640, 0);
+        //camera.update();
+    }
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 0, 0,2160,1280);
 		batch.end();
+        if(Gdx.input.isTouched()){
+            cameraX+=(Gdx.input.getX()-360);
+            if(cameraX<360)
+                cameraX=360;
+            if(cameraX>1800)
+                cameraX=1800;
+            camera.position.set(cameraX,640,0);
+        }
+        camera.update();
 	}
 }
